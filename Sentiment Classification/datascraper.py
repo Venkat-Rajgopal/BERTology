350ef39e5be6b45b02ff4d9ad6d7bf8e0d6783d0
venkat-rajgopal <venkatramani.r@gmail.com>
2020-09-22 21:04:04 +0200

    datascraper

--- Sentiment Classification/datascraper.py
+++ Sentiment Classification/datascraper.py
@@ -13,17 +13,17 @@ def read_texts(path, target):
 
     for dirname, dirnames, filenames in os.walk(path):
     # print path to all subdirectories first.
-    #    for subdirname in dirnames:
-    #        print(os.path.join(dirname, subdirname))
-
 
         for filename in tqdm(filenames,total = len(filenames)):      
             file = os.path.join(dirname, filename)
         
             with open(file, 'r', encoding="utf8") as f:
                 rev = f.readlines()
-                df = df.append({'review' : rev[0], 'sentiment' : target}, ignore_index = True)
+                df = df.append({'review': rev[0], 'sentiment': target}, ignore_index=True)
             
     return df
 
-read_texts(path = 'data/train/pos/', target = 'positive')
+
+df_trainpos = read_texts(path='data/train/pos/', target='positive')
+df_trainneg = read_texts(path='data/train/neg/', target='negative')
+
